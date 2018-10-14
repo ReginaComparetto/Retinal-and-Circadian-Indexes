946a79fc71a48de1bc68a420e4444ac08b327092
ReginaComparetto <38052135+ReginaComparetto@users.noreply.github.com>
2018-10-14 18:40:18 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -11,11 +11,8 @@ transmittBlue = RegulationTransmittBlueLight(measure);
 [TransmittUV, TransmittUVA, TransmittUVB] = RegulationTransmitUV(measure);
 [QRed, QYellow, QGreen, QBlue] = RegulationQrgby(measure);
 
-MCI_SD65 = MelanopsisCircadianIndexSD65(measure);
-RI_SD65 = RetinalIndexSD65(measure);
-RI_LCD = RetinalIndexLCD(measure);
-MCI_LCD = MelanopsisCircadianIndexLCD(measure);
-CI = CircadianIndex(measure);
+[RI_LCD RI_D65] = RetinalIndex(measure);
 [NLCI_ipad NLCI_D65] = NonLinearCircadianIndex(misure);
+[CII_ipad CII_D65] = CircadianImpactIndex(misure);
 
 ScotopicEfficiency = calcoloEffScotopica(measure);
